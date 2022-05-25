5935f1c7c29c7d375d81ad97508b666dc3acd285
Kalebe da Silva <craftanosclip74@gmail.com>
2022-05-25 16:51:17 -0300

    Finalizada a sessão portifólio, bastando adicionar as informações

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+/** Efeito Habilidades*/
 var number = document.querySelectorAll(".number")
 var counter = 0;
 setInterval(()=>{
@@ -12,3 +14,39 @@ setInterval(()=>{
     }
     
 },20)
+
+
+
+/**Slide show */
+const btns = document.querySelectorAll('.btn-slide');
+const slides = document.querySelectorAll('.video-slide');
+const contents = document.querySelectorAll('.portfolio-content');
+
+var slider = function(manual){
+    
+    btns.forEach((btn)=>{
+        btn.classList.remove("active")
+    });
+
+    slides.forEach((slide)=>{
+        slide.classList.remove("active")
+    });
+    
+    contents.forEach((content)=>{
+        content.classList.remove("active")
+    });
+
+
+    btns[manual].classList.add("active")
+    slides[manual].classList.add("active")
+    contents[manual].classList.add("active")    
+}
+btns.forEach((btn,i)=>{
+    btn.addEventListener("click",()=>{
+        slider(i);
+    })
+});
+
+
+
+
